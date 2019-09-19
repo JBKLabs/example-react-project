74b45d2ffefd264bdbf20c132508aac3dd33c0bd
Josh Hooton <hooton.josh@gmail.com>
2019-09-19 10:41:15 -0500

    Add eslint/prettier config files.
    Remove config from package.json, run linter.

--- src/index.js
+++ src/index.js
@@ -8,10 +8,10 @@ import store from './lib/store';
 import App from './App';
 
 ReactDOM.render(
-	<Provider store={store}>
-		<Router>
-			<App />
-		</Router>
-	</Provider>,
-	document.getElementById('root')
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
 );
--- src/views/Dashboard/Users/UserList.js
+++ src/views/Dashboard/Users/UserList.js
@@ -3,25 +3,25 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 const UserList = ({ users }) => (
-	<div>
-		<ul style={{ listStyle: 'none', paddingInlineStart: 0 }}>
-			{users.map((user) => (
-				<li key={user.id}>
-					{user.firstName} {user.lastName}
-				</li>
-			))}
-		</ul>
-	</div>
+  <div>
+    <ul style={{ listStyle: 'none', paddingInlineStart: 0 }}>
+      {users.map((user) => (
+        <li key={user.id}>
+          {user.firstName} {user.lastName}
+        </li>
+      ))}
+    </ul>
+  </div>
 );
 
 UserList.propTypes = {
-	users: PropTypes.arrayOf(
-		PropTypes.shape({
-			id: PropTypes.string,
-			firstName: PropTypes.string,
-			lastName: PropTypes.string
-		})
-	)
+  users: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      firstName: PropTypes.string,
+      lastName: PropTypes.string
+    })
+  )
 };
 
 const mapState = (state) => ({ users: state.users });
