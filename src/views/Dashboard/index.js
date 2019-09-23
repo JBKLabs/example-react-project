dda880b0827ec3062abf18245b95345f11ebf42c
Brian Maule <12001504+mauleb@users.noreply.github.com>
2019-09-23 16:47:58 -0500

    add templating, fill out README

--- src/views/Dashboard/index.js
+++ src/views/Dashboard/index.js
@@ -6,6 +6,7 @@ import Users from './Users';
 const Dashboard = () => (
   <>
     <h1>Example React App</h1>
+    {window.env.ENABLE_DEBUG_MODE === 'true' && <div>debug mode enabled</div>}
     <div style={{ display: 'flex', flexDirection: 'column' }}>
       <CurrentTime />
       <Users />
