76d0e915b49f83a61e12b11f80f08c2aa176fe2d
Brian Maule <12001504+mauleb@users.noreply.github.com>
2019-11-22 15:37:33 -0600

    use core-js instead of @babel/polyfill

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ModelProvider } from '@jbknowledge/react-models';
-import '@babel/polyfill';
+import 'core-js/stable';
 
 import App from './App';
 
